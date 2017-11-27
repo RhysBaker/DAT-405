e8286e86981d12a45bc283bcfd8fb4b79843f377
Rhys Baker <rhys.baker99@gmail.com>
2017-11-27 17:26:08 +0000

    radom coloured squares inside squares

--- Class Github/Session3/03_Nested_For-Loop_Visual/sketch.js
+++ Class Github/Session3/03_Nested_For-Loop_Visual/sketch.js
@@ -12,15 +12,15 @@ function setup() {
 }
 
 function draw() {
-  fill(215, 200, 170);
-
   //Run the nested loop for x, and y
   for (let y = 0; y < 10; y++) {
     for (let x = 0; x < 10; x++) {
-      //Draw the shape at the corresponding position
-      //This will generate a dynamic position for the x and y
-      //The shape is redrawn as many times as the loop says (in this case 100)
+      fill(215, 200, 170);
       rect(size * x, size * y, size, size);
+
+      fill(random(255), random(255), random(255));
+      rect(size * x + 13, size * y + 13, 25, 25);
+
     }
   }
 }
