800762cb1120e930646d762c5a57779ccbec3289
RhysBaker <rhys.baker99@gmail.com>
2017-11-28 14:10:04 +0000

    fixed cc

--- Class Github/Session3/Colour Coordinates/sketch.js
+++ Class Github/Session3/Colour Coordinates/sketch.js
@@ -1,110 +1,29 @@
-var rectColour;
-var rectColour2;
-var rectColour3;
-var rectColour4;
-var overRect;
-var overRect2;
-var overRect3;
-var overRect4;
+
 
 function setup() {
-  // set canvas size
-  createCanvas(windowWidth, windowHeight);
-  frameRate(15);
-
-  // default background color
-  backgroundColor = color(255, 255, 255);
+  createCanvas(500, 500);
+  backgroundColor = color(0, 0, 0);
 }
 
 function draw() {
   background(backgroundColor);
 
+  var mouse1 = map(mouseX, 0, width, 0, 255);
+  var mouse2 = map(mouseY, 0, height, 0, 255);
+  var mouse3 = map(mouseX, 0, width, 255, 0);
+  var mouse4 = map(mouseY, 0, width, 255, 0);
 
-  // check if mouse is inside the rectangle
-  // mouseX >= x && mouseX <= x+width && mouseY >= y && mouseY <= y+height
-  if (mouseX >= 110 && mouseX <= 110+100 && mouseY >= 110 && mouseY <= 110+100) {
-    overRect = true;
-  }
-    else {
-    overRect = false;
-  }
+r = color(255,0,0, mouse1);
+g = color(0,255,0, mouse2);
+b = color(0,0,255, mouse3);
+y = color(255,255,0, mouse4);
 
-    if (mouseX >= 110 && mouseX <= 110+100 && mouseY >= 10 && mouseY <= 10+100) {
-    overRect2 = true;
-  }
-    else {
-    overRect2 = false;
-  }
-
-    if (mouseX >= 10 && mouseX <= 10+100 && mouseY >= 10 && mouseY <= 10+100) {
-    overRect3 = true;
-  }
-    else {
-    overRect3 = false;
-  }
-
-     if (mouseX >= 10 && mouseX <= 10+100 && mouseY >= 110 && mouseY <= 110+100) {
-    overRect4 = true;
-  }
-    else {
-    overRect4 = false;
-  }
-
-  // draw a rectangle
-
-  rectMode(CORNER);
-  stroke(0);
-  strokeWeight(5);
-  rect(10, 10, 100, 100);
-
-
-    // draw a rectangle
-
-  rectMode(CORNER);
-  stroke(0);
-  strokeWeight(5);
-  rect(10, 110, 100, 100);
-
-
-    // draw a rectangle
-
-  rectMode(CORNER);
-  stroke(0);
-  strokeWeight(5);
-  rect(110, 10, 100, 100);
-
-
-
-    // draw a rectangle
-
-  rectMode(CORNER);
-  stroke(0);
-  strokeWeight(5);
-  rect(110, 110, 100, 100);
-
-
-}
-
-function mouseMoved()
-{
-  if(overRect == true)
-  {
-    rectColour = fill(random(255), random(255), random(255));
-  }
-
-    if(overRect2 == true)
-  {
-    rectColour2 = fill(random(255), random(255), random(255));
-  }
-
-    if(overRect3 == true)
-  {
-    rectColour3 = fill(random(255), random(255), random(255));
-  }
-
-    if(overRect4 == true)
-  {
-    rectColour4 = fill(random(255), random(255), random(255));
-  }
-
+fill(r);
+  var rect1 = rect(0,0,250,250);
+  fill(g);
+  var rect2 = rect(250,0,250,250);
+  fill(b);
+  var rect3 = rect(250,250,250,250);
+  fill(y)
+  var rect4 = rect(0,250,250,250);
 }
