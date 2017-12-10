ec2486d41eb805dba86c22fd4359367f016f6e6d
Rhys Baker <rhys.baker99@gmail.com>
2017-12-10 23:40:17 +0000

    update

--- Assignment 1/sketch.js
+++ Assignment 1/sketch.js
@@ -5,43 +5,47 @@ var rSlider, gSlider, bSlider;
 
 
 function setup() {
-  createCanvas(450, 450);
+var canvas = createCanvas(594, 841);
+canvas.class("canvasContainer");
+canvas.parent("canvasContainer");
   rSlider = createSlider(0, 255, 255);
   rSlider.position(200, 100);
   gSlider = createSlider(0, 255, 255);
   gSlider.position(200, 130);
   bSlider = createSlider(0, 255, 255);
-  bSlider.position(200  , 160);
-  for (let i=0; i<arraySize; i++){
+  bSlider.position(200, 160);
+  for (let i = 0; i < arraySize; i++) {
     circleArray[i] = new Circle;
   }
 }
 
 
 function draw() {
+
   var r = rSlider.value();
   var g = gSlider.value();
   var b = bSlider.value();
-  text("red", rSlider.x * 2 + rSlider.width, 35);
-  text("green", gSlider.x * 2 + gSlider.width, 65);
-  text("blue", bSlider.x * 2 + bSlider.width, 95);
-  background(r,g,b);
-  for (let i=0; i<circleArray.length; i++){
+  background(r, g, b,20);
+
+
+
+  for (let i = 0; i < circleArray.length; i++) {
     circleArray[i].moveFunction();
     circleArray[i].displayCircle();
 
     if (mouseIsPressed) {
-    for (let i=0; i<arraySize; i++){
-      circleArray[i] = new Circle(mouseX, mouseY, random(-1, 1), random(-1, 1), random(1, 25));
+      for (let i = 0; i < arraySize; i++) {
+        circleArray[i] = new Circle(mouseX, mouseY, random(-2, 2), random(-2, 2), random(1, 25));
+      }
     }
+
   }
-}
 }
 
 //Definition of the class Circle
-class Circle{
+class Circle {
 
-  constructor(x, y, speedX, speedY, size){
+  constructor(x, y, speedX, speedY, size) {
     //Setup of class' variables
     this.x = x;
     this.y = y;
@@ -56,22 +60,28 @@ class Circle{
   }
 
   //Class function that takes care of motion and collision
-  moveFunction(){
+  moveFunction() {
     //Motion system - current position and speed
     this.x = this.x + this.speedX;
     this.y = this.y + this.speedY;
 
     //Based on boundaries collision, reverse direction for x and y
-    if (this.x > width || this.x<0){
-      this.speedX *= -1;
-    }
-    if (this.y > (height) || this.y<0){
-      this.speedY *= -1;
+    if (keyIsDown(LEFT_ARROW)) {
+      if (this.x > width || this.x < 0) {
+        this.speedX *= -1;
+      }
+      if (this.y > (height) || this.y < 0) {
+        this.speedY *= -1;
+      }
+    } else {
+      if (this.x > width || this.x < 0) {}
+      if (this.y > (height) || this.y < 0) {}
     }
   }
 
+
   //Class function that displays the ellipse
-  displayCircle(){
+  displayCircle() {
     noStroke();
     this.fillcol = color(this.rd, this.grn, this.bl, this.a)
     fill(this.fillcol);
